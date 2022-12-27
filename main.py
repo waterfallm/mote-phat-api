7aca07f1a3945f9b16acce840e08a7a7b8aa8536
waterfallm <matthew@waterfall-family.co.uk>
2022-12-27 13:19:15 +0000

    motephat

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import Body, FastAPI
 import sys
 import time
 
-# import motephat
+import motephat
 
 
-# motephat.set_brightness(1)
-# motephat.set_clear_on_exit(True)
+motephat.set_brightness(1)
+motephat.set_clear_on_exit(True)
 
 def usage():
     print("Usage: {} <r> <g> <b>".format(sys.argv[0]))
@@ -36,6 +36,16 @@ async def rgb(payload: dict = Body(...)):
     # Exit if any of r, g, b are greater than 255
     if max(r, g, b) > 255:
         return {"message": "RGB should be Integager value between 0-255" }
+
+
+    while True:
+
+        for channel in range(4):
+            for pixel in range(16):
+                motephat.set_pixel(channel + 1, pixel, r, g, b)
+            time.sleep(0.01)
+
+    motephat.show()
 
     # return {"message": "RGB set R:" + {payload['R']} }
     return {"message": "RGB set RGB"}
