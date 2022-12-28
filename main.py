a0db0f14b94e69fe92cbfde39b0f1a0af0602355
waterfallm <matthew@waterfall-family.co.uk>
2022-12-28 14:46:08 +0000

    tasks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import Body, FastAPI, Request, logger , Form
+from fastapi import Body, FastAPI, Request, logger , Form, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 # from fastapi.templating import Jinja2Templates
@@ -19,6 +19,17 @@ motephat.set_brightness(1)
 motephat.set_clear_on_exit(True)
 
 app = FastAPI()
+
+def write_rgb(red: int, blue: int,green: int,):
+    while True:
+
+        for channel in range(4):
+            for pixel in range(16):
+                motephat.set_pixel(channel + 1, pixel, red, blue, green)
+            time.sleep(0.01)
+
+    motephat.show()
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -66,43 +77,21 @@ async def rgb(payload: Request = Body(...)):
 
 
 @app.get("/rgb/red")
-async def rgb(payload: Request = Body(...)):
+async def rgb(background_tasks: BackgroundTasks):
 
-    while True:
-
-        for channel in range(4):
-            for pixel in range(16):
-                motephat.set_pixel(channel + 1, pixel, 255, 0, 0)
-            time.sleep(0.01)
-
-    motephat.show()
-
+    background_tasks.add_task(write_rgb, 255, 0, 0)
     return {"message": "RGB set Red"}
     
 @app.get("/rgb/green")
-async def rgb(payload: Request = Body(...)):
+async def rgb(background_tasks: BackgroundTasks):
 
-    while True:
-
-        for channel in range(4):
-            for pixel in range(16):
-                motephat.set_pixel(channel + 1, pixel, 0, 255, 0)
-            time.sleep(0.01)
-
-    motephat.show()
+    background_tasks.add_task(write_rgb, 0, 0, 255)
 
     return {"message": "RGB set Green"}
 
 @app.get("/rgb/blue")
-async def rgb(payload: Request = Body(...)):
+async def rgb(background_tasks: BackgroundTasks):
 
-    while True:
-
-        for channel in range(4):
-            for pixel in range(16):
-                motephat.set_pixel(channel + 1, pixel, 0, 0, 255)
-            time.sleep(0.01)
-
-    motephat.show()
+    background_tasks.add_task(write_rgb, 0, 255, 0)
 
     return {"message": "RGB set Blue"}
